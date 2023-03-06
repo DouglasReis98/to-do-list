f749a44de2fcb4bfcdc71a6fe1c77a3986150ec9
DouglasReis <douglasamaralreis@outlook.com>
2023-03-06 11:52:10 -0300

    update

--- js/script.js
+++ js/script.js
@@ -6,26 +6,28 @@ let containerTarefas = document.getElementById("tarefa-container");
 btnNovaTarefa.addEventListener("click", () => {
     if (novaTarefa.style.display = "none") {
         novaTarefa.style.display = "block"
-
     }
 })
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    let tarefas = []
+    let tarefas = [];
 
     if (localStorage.getItem("outTarefa")) {
 
-    tarefas = JSON.parse(localStorage.getItem("outTarefa"));
+        tarefas = JSON.parse(localStorage.getItem("outTarefa"));
 
     }
     const tarefa = frm.inNovaTarefa.value;
     tarefas.push(tarefa);
     localStorage.setItem("outTarefa", JSON.stringify(tarefas));
     mostrarTarefas();
-    frm.reset()
+    frm.reset();
+    novaTarefa.style.display = "none";
 })
+
+
 
 function mostrarTarefas() {
     if (!JSON.parse(localStorage.getItem("outTarefa"))) {
@@ -34,30 +36,19 @@ function mostrarTarefas() {
                                         </article>`
     } else {
 
-        const outTarefa = JSON.parse(localStorage.getItem("outTarefa"));
+        const tarefas = JSON.parse(localStorage.getItem("outTarefa"));
         //console.log(outTarefa)
         containerTarefas.innerHTML = ""
-        for (tarefa of outTarefa) {
-
+        for (let i = 0; i < tarefas.length; i++) {
             let article = document.createElement("article");
             article.className = 'border border-info m-2'
-            article.innerHTML = `<h5>${tarefa}</h5>
-            <div class="controls m-1">
-                <i class="fa-regular fa-circle-check btn btn-success"></i>
-                <i class="fa-solid fa-pencil btn btn-warning"></i>
-                <i class="fa-solid fa-trash btn btn-danger"></i>
-            </div>`;
-            containerTarefas.append(article)
-            /*
-            containerTarefas.innerHTML = `<article class="border border-info m-2">
-                                <h5>${tarefa}</h5>
+            article.innerHTML = `<h5>${tarefas[i]}</h5>
                                     <div class="controls m-1">
                                         <i class="fa-regular fa-circle-check btn btn-success"></i>
                                         <i class="fa-solid fa-pencil btn btn-warning"></i>
                                         <i class="fa-solid fa-trash btn btn-danger"></i>
-                                    </div>
-                            </article>`
-*/
+                                    </div>`;
+            containerTarefas.append(article)
         }
     }
 
