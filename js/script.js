a41cf208ccbd2cd700f0765e1a34280cb4a0a30b
DouglasReis <douglasamaralreis@outlook.com>
2023-03-10 21:13:09 -0300

    excluir-tarefa

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let btnNovaTarefa = document.getElementById("btnNovaTarefa");
+let btnExcluirTarefa = document.getElementById("excluir-tarefa");
 let novaTarefa = document.getElementById("novaTarefa");
 let frm = document.querySelector("form");
 let containerTarefas = document.getElementById("tarefa-container");
@@ -20,11 +21,14 @@ frm.addEventListener("submit", (e) => {
 
     }
     const tarefa = frm.inNovaTarefa.value;
-    tarefas.push(tarefa);
-    localStorage.setItem("outTarefa", JSON.stringify(tarefas));
-    mostrarTarefas();
-    frm.reset();
-    novaTarefa.style.display = "none";
+    
+    if (tarefa != "") {
+        tarefas.push(tarefa);
+        localStorage.setItem("outTarefa", JSON.stringify(tarefas));
+        mostrarTarefas();
+        frm.reset();
+        novaTarefa.style.display = "none";
+    }
 })
 
 
@@ -34,6 +38,7 @@ function mostrarTarefas() {
         containerTarefas.innerHTML = `<article class="border border-info m-2">
                                         <h5>Não há tarefas cadastradas aqui!!!</h5>
                                         </article>`
+        localStorage.clear();
     } else {
 
         const tarefas = JSON.parse(localStorage.getItem("outTarefa"));
@@ -44,14 +49,33 @@ function mostrarTarefas() {
             article.className = 'border border-info m-2'
             article.innerHTML = `<h5>${tarefas[i]}</h5>
                                     <div class="controls m-1">
-                                        <i class="fa-regular fa-circle-check btn btn-success"></i>
-                                        <i class="fa-solid fa-pencil btn btn-warning"></i>
-                                        <i class="fa-solid fa-trash btn btn-danger"></i>
+                                        <i onclick="tarefaConcluida(${[i]})" class="fa-regular fa-circle-check btn btn-success"></i>
+                                        <i onclick="editarTarefa(${[i]})" class="fa-solid fa-pencil btn btn-warning"></i>
+                                        <i onclick="excluirTarefa(${[i]})" class="fa-solid fa-trash btn btn-danger"></i>
                                     </div>`;
             containerTarefas.append(article)
         }
     }
 
 }
+
+function tarefaConcluida(i) {
+    console.log("Tarefa Concluída" + [i]);
+};
+
+function editarTarefa(i) {
+    console.log("Editar Tarefa" + [i]);
+};
+
+function excluirTarefa(i) {
+    let mensagem = "Deseja excluir esta tarefa?"
+    if (confirm(mensagem) == true) {    
+        let tarefas = [];
+        tarefas = JSON.parse(localStorage.getItem("outTarefa"));
+        tarefas.splice(i, 1);
+        localStorage.setItem("outTarefa", JSON.stringify(tarefas));
+        mostrarTarefas();
+    }
+};
 
 window.addEventListener("load", mostrarTarefas)
